3c126ddb4562d6819e705a1f17cb83f8cff1e509
Basmala Mohamed <Basmala01067618897@gmail.com>
2025-10-15 21:13:16 +0300

    Add: Sign in Sign up Screen

--- app/src/main/java/com/moviecoo/colorthemeandtypography/ui/theme/Type.kt
+++ app/src/main/java/com/moviecoo/colorthemeandtypography/ui/theme/Type.kt
@@ -2,10 +2,15 @@ package com.moviecoo.colorthemeandtypography.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.moviecoo.colorthemeandtypography.R
 
+val RomanescoFontFamily = FontFamily(
+    Font(R.font.romanesco_regular, FontWeight.Normal),
+)
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
