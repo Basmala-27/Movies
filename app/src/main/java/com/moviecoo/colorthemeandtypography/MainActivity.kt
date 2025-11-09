34ffade49e86f96cee567874f4f8d2f7ea640211
Asmaa <asmasyd746@gmail.com>
2025-11-09 16:35:53 +0200

    Add MovieBottomAppBarEdited new verson

--- app/src/main/java/com/moviecoo/colorthemeandtypography/MainActivity.kt
+++ app/src/main/java/com/moviecoo/colorthemeandtypography/MainActivity.kt
@@ -2,14 +2,36 @@ package com.moviecoo.colorthemeandtypography
 
 
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.moviecoo.colorthemeandtypography.common_components.AnimatedBottomBar
 import com.moviecoo.colorthemeandtypography.navigation.AppNavHost
 import com.moviecoo.colorthemeandtypography.ui.screens.splashScreen.SplashScreen
 import com.moviecoo.colorthemeandtypography.ui.theme.ColorThemeandTypographyTheme
 
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.S)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
       //  enableEdgeToEdge()
@@ -23,6 +45,35 @@ class MainActivity : ComponentActivity() {
 //
 //                    }
 //                )
+                var feedItemCount by remember { mutableStateOf(0) }
+                Scaffold(
+                    containerColor = Color(0xFF0E0E0E),
+                    bottomBar = {
+                        AnimatedBottomBar(
+                            feedItemCount = feedItemCount,
+                            modifier = Modifier.padding(bottom = 65.dp)
+                        )
+                    }
+                ) { innerPadding ->
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                            Text(
+                                text = "Main Screen Content",
+                                color = Color.White,
+                                fontSize = 20.sp
+                            )
+                            Spacer(Modifier.height(16.dp))
+                            Button(onClick = { feedItemCount++ }) {
+                                Text("Add to Feed (Count: $feedItemCount)")
+                            }
+                        }
+                    }
+                }
                 AppNavHost()
             }
         }
