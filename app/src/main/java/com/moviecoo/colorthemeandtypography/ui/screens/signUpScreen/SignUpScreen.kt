3925d21a2a751d15812194ee7c61009a898889dc
COMPUMARTS <zozatah@gmail.com>
2025-11-12 02:03:16 +0200

    firebase

--- app/src/main/java/com/moviecoo/colorthemeandtypography/ui/screens/signUpScreen/SignUpScreen.kt
+++ app/src/main/java/com/moviecoo/colorthemeandtypography/ui/screens/signUpScreen/SignUpScreen.kt
@@ -1,4 +1,4 @@
-package com.moviecoo.colorthemeandtypography.ui.screens.signInScreen
+package com.moviecoo.colorthemeandtypography.ui.screens.signUpScreen
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
